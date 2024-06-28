2e0f1345689af96daa5930779313a4303c7bf138
Piyush Chauhan <158813985+PiyushChauhan3@users.noreply.github.com>
2024-06-28 22:22:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,17 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
-document.addEventListener("DOMContentLoaded", function() {
-  document.getElementById("thq-button-filled cta1-button").addEventListener("click", function() {
-    window.location.href = "http://localhost/Transportation%20MS//login-signup.php";
-  });
-});
 
-document.addEventListener("DOMContentLoaded", function() {
-  document.getElementById("hero3-button thq-button-filled").addEventListener("click", function() {
-    window.location.href = "http://localhost/Transportation%20MS//login-signup.php";
-  });
-});
 
 document.addEventListener("DOMContentLoaded", function() {
   document.getElementsByClassName("btn btn-primary btn-lg btn-block").addEventListener("click", function() {
